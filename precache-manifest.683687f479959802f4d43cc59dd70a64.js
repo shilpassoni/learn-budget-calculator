8d0e067c3cc4e9892dd939b4b6e9296ea3b05fd3
Shilpa Soni <shilpassoni@gmail.com>
2020-04-08 16:41:38 +0530

    Updates

---
+++ precache-manifest.683687f479959802f4d43cc59dd70a64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ec69ddd6ab9d211e496df745b36f92",
+    "url": "/learn-budget-calculator/index.html"
+  },
+  {
+    "revision": "d561351b9b7dc967111e",
+    "url": "/learn-budget-calculator/static/css/main.657c7433.chunk.css"
+  },
+  {
+    "revision": "a312d0d2519e48d3b1ee",
+    "url": "/learn-budget-calculator/static/js/2.873e7328.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learn-budget-calculator/static/js/2.873e7328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d561351b9b7dc967111e",
+    "url": "/learn-budget-calculator/static/js/main.5f73400f.chunk.js"
+  },
+  {
+    "revision": "73bc02a1bd01360d10cd",
+    "url": "/learn-budget-calculator/static/js/runtime-main.a5df47bc.js"
+  }
+]);
